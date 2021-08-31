661dee98d55a4f7bb49c081e30d77ca782f2b15e
keigotoyo <keigo.work199987s@gmail.com>
2021-08-31 10:33:05 +0900

    base

---
+++ english_script_devided_app/frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import React,{Component, components} from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="center">
+        <HomePage/>
+      </div>
+    );
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App name="keigo"/>, appDiv);

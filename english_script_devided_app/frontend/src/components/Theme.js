6c1e8ea83ca3d27b0c1aa5bbb8ac67b24ea7b719
keigotoyo <keigo.work199987s@gmail.com>
2021-09-20 15:06:24 +0900

    remove __pycache__

--- english_script_devided_app/frontend/src/components/Theme.js
+++
@@ -1,9 +0,0 @@
-import { createTheme } from "@material-ui/core";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: '"Montserrat", Open Sans',
-  },
-});
-
-export default theme;
--- english_script_devided_app/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path('', index),
-    path('movie', index),
-    path('drama', index),
-    # path('room/<str:roomCode>', index)
-]

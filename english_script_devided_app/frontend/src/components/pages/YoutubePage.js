171a61cb640cb31fd0d477723af0e847ecda562c
keigotoyo <keigo.work199987s@gmail.com>
2022-03-02 09:20:09 +0900

    embed #21

--- english_script_devided_app/frontend/src/components/pages/YoutubePage.js
+++ english_script_devided_app/frontend/src/components/pages/YoutubePage.js
@@ -11,6 +11,7 @@ import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import CssTextField from "../theme/MuiThemeTextField";
+import { Row, Col, Container } from "react-bootstrap";
 
 
 const styles = {
@@ -23,22 +24,52 @@ const styles = {
   }
 };
 
+const base_url = "https://www.youtube.com/embed/"
 
 
 export default class YoutubePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputValue: '',
+      src:"",
+      video_id:"",
     };
 
-    // this.getListMovie();
+  }
+  componentDidMount(){
+    console.log("componentDidMount()"); 
   }
   getAttribute() {
-    var d = document.getElementById("video");
-    var v = "vhhgI4tSMwc";
-    var src = `https://www.youtube.com/embed/${v}`;
-    console.log("src: %s", src);
-    d.setAttribute("src", src);
+    console.log(this.state.video_id);
+    let src = base_url + this.state.video_id;
+    console.log(src);
+    this.setState({ src: src });
+  }
+
+  updateInputValue(evt) {
+    const val = evt.target.value;
+    this.setState({
+      inputValue: val
+    });
+    console.log("update");
+  }
+  /* なぜか通常のfunctionで定義するとコールバック関数が実行されるときにthisが渡されない．単純にonFormSubmit関数が実行される． アローfunctionに変えてあげるとthisが自動的に渡される*/
+  onSubmit = (e) => {
+    console.log('Click');
+    e.preventDefault();
+    const url = new URL(this.state.inputValue);
+    const params = new URLSearchParams(url.search);
+    for (let param of params) {
+      if (param[0] == "v"){
+        this.setState();
+        this.setState({ video_id: param[1] }, () => {
+          console.log(this.state.video_id, 'this.state.video_id1');
+          this.getAttribute();
+
+        });  
+      }
+    }
   }
 
   render() {
@@ -49,11 +80,27 @@ export default class YoutubePage extends Component {
         </Grid>
         <Grid item xs={8}>
           <div className="row">
-            <div className="row">
-              <CssTextField id="outlined-basic" label="URL" variant="outlined" size='small' margin="normal"/>
-            </div>
+
+              <form onSubmit={this.onSubmit}>
+
+                <Container className="mt-4">
+                  <Row>
+                    <Col xs={9}>
+                    <CssTextField id="outlined-basic" style={{ width: '100%' }} label="URL" variant="outlined" size='small'  value={this.state.inputValue} onChange={e => this.updateInputValue(e)}/>
+                    </Col>
+                    <Col xs={1}>
+                      <Button variant="outlined" type="submit" margin="normal">
+                        Confirm
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+
+              </form>
+
             <div className='row'>
-              <iframe id="video" width="560" height="315" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+             {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/DINxNbBOEoI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+              <iframe id='src' width="560" height="315" src={this.state.src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
             <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
               <div className="script">

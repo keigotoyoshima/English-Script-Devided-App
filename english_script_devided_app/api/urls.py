6e005b67ec40c5402f474b9f7294a28b4d65b219
keigotoyo <keigo.work199987s@gmail.com>
2022-04-03 16:48:44 +0900

    #87 #84 #83 #82 #80

--- english_script_devided_app/api/urls.py
+++ english_script_devided_app/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import GetYoutubeTranscript,  movie_api_view,user_api_view, word_api_view
 
-
 urlpatterns = [
     path('get-youtube-transcript/', GetYoutubeTranscript.as_view()),
     path('user-get/<str:displayName>/', user_api_view),
@@ -9,5 +8,7 @@ urlpatterns = [
     path('word-get/<str:displayName>/<str:v>/', word_api_view),
     path('word-post/<str:displayName>/<str:v>/', word_api_view),
     path('movie-get/<str:displayName>/', movie_api_view),
-    path('movie-post/<str:displayName>/', movie_api_view)
+    path('movie-post/<str:displayName>/', movie_api_view),
+    path('movie-put/<str:displayName>/', movie_api_view),
+    path('movie-delete/<str:displayName>/<str:v>/', movie_api_view),
 ]
--- english_script_devided_app/english_script_devided_app/settings.py
+++ english_script_devided_app/english_script_devided_app/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

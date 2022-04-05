3a11410b7a1fe7d248012b58844adb79e179e52a
keigotoyo <keigo.work199987s@gmail.com>
2022-04-05 14:17:49 +0900

    AWS_S3_CUSTOM_DOMAIN

--- english_script_devided_app/english_script_devided_app/settings.py
+++ english_script_devided_app/english_script_devided_app/settings.py
@@ -150,8 +150,9 @@ else:
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
 

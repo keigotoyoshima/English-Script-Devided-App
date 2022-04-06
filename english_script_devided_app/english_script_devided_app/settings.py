7154f815f248e88cdf824311c49161ddaedf2612
keigotoyo <keigo.work199987s@gmail.com>
2022-04-06 10:32:09 +0900

    s

--- english_script_devided_app/english_script_devided_app/settings.py
+++ english_script_devided_app/english_script_devided_app/settings.py
@@ -147,11 +147,11 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = 'https://s3.amazonaws.com/%s/static' % AWS_STORAGE_BUCKET_NAME
     # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     # AWS_LOCATION = 'static'
     # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # https: // s3.amazonaws.com/youtubelanguage.s3.bucket/static/admin/css/autocomplete.css
     

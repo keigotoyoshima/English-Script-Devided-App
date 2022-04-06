edc83c3b72b8026933151b1e9cbfcf9347d377e7
keigotoyo <keigo.work199987s@gmail.com>
2022-04-06 11:04:26 +0900

    s

--- english_script_devided_app/english_script_devided_app/settings.py
+++ english_script_devided_app/english_script_devided_app/settings.py
@@ -143,11 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiless')
-# if not DEBUG:
-#     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-#     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # STATIC_URL = 'https://s3.amazonaws.com/%s/static' % AWS_STORAGE_BUCKET_NAME
     # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     # AWS_LOCATION = 'static'

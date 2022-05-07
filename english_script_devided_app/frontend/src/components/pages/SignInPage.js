ef14b580ba2f5c95f67164238165413f7817949b
keigotoyo <keigo.work199987s@gmail.com>
2022-05-07 10:49:53 +0900

    yarn run build

--- english_script_devided_app/frontend/src/components/pages/SignInPage.js
+++ english_script_devided_app/frontend/src/components/pages/SignInPage.js
@@ -71,7 +71,7 @@ export default function SignInPage(props) {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in 2
+            Sign in 3
           </Typography>
           <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
             <TextField

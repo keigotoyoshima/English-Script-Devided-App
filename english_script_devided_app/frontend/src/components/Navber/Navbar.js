fff1ffa63927dd4f2b6a1734fbed81062338eb99
keigotoyo <keigo.work199987s@gmail.com>
2022-05-22 13:43:42 +0900

    大規模改修 #136 #135 #134 #133  #131 #117 #115

--- english_script_devided_app/frontend/src/components/Navber/Navbar.js
+++ english_script_devided_app/frontend/src/components/Navber/Navbar.js
@@ -4,8 +4,16 @@ import { NavDropdown } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
 import { useUserContext } from "../userContext/userContext";
 import { IP_AND_PORT } from "../frontend_api/DjangoApi";
+import { TextField } from "@mui/material";
+import { Button } from "react-bootstrap";
 
-const AppNavBar = () => {
+
+const AppNavBar = ({
+  labelURL,
+  setInputURL,
+  inputURL,
+  onSubmit,
+}) => {
   const { logoutUser, user, displayName } = useUserContext();
 
 
@@ -17,21 +25,55 @@ const AppNavBar = () => {
     logoutUser();
   }
 
+  // URLinput
+  const updateInputValue = (evt) => {
+    const val = evt.target.value;
+    setInputURL(val)
+  }
+
   return (
-    <Navbar bg="dark" variant="dark" style={{ display: "block" }}>
+    <Navbar style={{ display: "block", backgroundColor: "#202020", height:"6%"}}>
       <Row>
-        <Col xs={6} >
-          <Navbar.Brand href="#home" style={{ marginLeft: "2rem", display: "block" }}>Youtube Language</Navbar.Brand>
+        <Col xs={2} >
+          <Navbar.Brand href="#home" style={{ fontSize: "1.2vw", height: "80%", marginLeft: "2rem", padding: "0.3rem", display: "flex", alignItems: "center",  color: "#FAFAFA", borderRadius: "10px", backgroundColor: "#0F80D7",  
+          position: "absolute", top:"50%", transform:"translate(0, -50%)" }}>Youtube Language</Navbar.Brand>
         </Col>
-        <Col xs={6}>
+        <Col xs={7} >
+          <form onSubmit={onSubmit}>
+            <Row style={{ height: "100%" }}>
+              <Col xs={10} style={{ paddingRight: "0" }}>
+                <TextField sx={{
+                  height: "90%",
+                  width: "100%",
+                  top: "-5px",
+                  "& .MuiInput-underline:before": {
+                    borderBottomColor: "#888888"
+                  },
+                  "& .MuiInput-underline:after": {
+                    borderBottomColor: "#FAFAFA"
+                  }
+                }} label={labelURL} error={labelURL != "URL"} value={inputURL} onChange={e => updateInputValue(e)} variant="standard" inputProps={{ style: { fontSize: 15, color: "#FAFAFA" } }} InputLabelProps={{ style: { fontSize: 15, color: "#888888", padding: "0", margin: "0", height: "100%", lineHeight: "1" } }} />
+              </Col>
+              <Col xs={2} style={{ paddingLeft: "0" }}>
+                <Button style={{ width: "5rem", display: "block", position: "absolute", top: "3px", backgroundColor: "#313131", color: "#FAFAFA" }} className="react-button" variant="outlined" type="submit" margin="normal">
+                  SET
+                </Button>
+              </Col>
+            </Row>
+          </form>
+          
+          
+         
+        </Col>
+        <Col xs={3}>
           <Row style={{height:"100%"}}>
-            <Col xs={10} style={{ justifyContent: "end", alignItems: "center", display:"flex"}}>
-              <Navbar.Text style={{fontSize:"larger", paddingTop:"0.6rem  "}}>
+            <Col xs={10} style={{ justifyContent: "center", alignItems: "center", display:"flex"}}>
+              <Navbar.Text style={{ fontSize: "medium", paddingTop: "0.6rem", color: "#888888"}}>
                 Signed in as: {displayName}
               </Navbar.Text>
             </Col>
             <Col xs={2} style={{ justifyContent: "end", alignItems:"center",  display: "flex" }}>
-              <NavDropdown title="Options" id="basic-nav-dropdown" style={{ fontSize: "larger" }}>
+              <NavDropdown title="Options" id="basic-nav-dropdown" style={{ fontSize: "medium" }}>
                 <NavDropdown.Item onClick={onClickForManagePage}>Manage Page</NavDropdown.Item>
                 <NavDropdown.Item onClick={onClickForLogout}>Logout</NavDropdown.Item>
                 <NavDropdown.Item>Cancel</NavDropdown.Item>

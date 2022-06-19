29ccb15901ede67994c2c9b57bfa9b2b69695c6f
keigotoyo <keigo.work199987s@gmail.com>
2022-06-19 15:18:10 +0900

    コメント除去

--- english_script_devided_app/frontend/src/components/frontend_api/DictionaryApi.js
+++ english_script_devided_app/frontend/src/components/frontend_api/DictionaryApi.js
@@ -4,7 +4,6 @@ const dictionaryApi = async (language, word) => {
   try {
     const data = await axios.get(
       `https://api.dictionaryapi.dev/api/v2/entries/${language}/${word}`
-      // `https://api.dictionaryapi.dev/api/v2/entries/en/plane`
     );
     return data.data
   } catch (error) {

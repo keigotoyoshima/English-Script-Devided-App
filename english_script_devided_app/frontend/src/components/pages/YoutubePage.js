db1bf0fc492c63e6cd5c1c78936e8f5f4bce3d3e
keigotoyo <keigo.work199987s@gmail.com>
2022-07-02 10:45:01 +0900

    リファクタリング

--- english_script_devided_app/frontend/src/components/pages/YoutubePage.js
+++ english_script_devided_app/frontend/src/components/pages/YoutubePage.js
@@ -25,7 +25,6 @@ const YoutubePage = () => {
   const [inputURL, setInputURL] = useState("")
   const [labelURL, setLabelURL] = useState("URL")
   const [video_id, setVideo_id] = useState("")
-  const [title, setTitle] = useState("")
   const [transcription_list, setTranscription_list] = useState([])
   const [display_transcription, setDisplay_transcription] = useState(true)
   const [twoDimensionalArray, setTwoDimensionalArray] = useState([[]])
@@ -73,39 +72,12 @@ const YoutubePage = () => {
   }, []);
 
   useEffect(() => {
-    console.log(displayName, 'displayName');
-
     setMovie_list([]);
     setVocabulary_list([]);
     getSavedMovies();
   }, [displayName]);
 
-  // とりあえずここにおく
-  const putHeapSavedMovie = (editValue, v) => {
-    let index = mapping[v];
-    let data = {title:editValue, v: v};
-    movie_list_unregisterd[index] = data;
-    let list = movie_list_unregisterd.slice();
-    setMovie_list(list); 
-  }
-
-  const callYoutubeDataApi = async (v) => {
-    const api_endpoint = `https://vw0sqj3qmc.execute-api.us-east-1.amazonaws.com/stage_api?v=${v}`;
-
-    axios.get(api_endpoint)
-      .then((response) => {
-        if (response.data.body.error) {
-          console.log("error: ", response.data.body.message);
-        } else {
-          let title = response.data.body.title;
-          postMovie(title, v);
-        }
-      })
-      .catch((error) => {
-        console.log("error: ", error);
-      })
-  }
-
+  
   // video_idが更新されたタイミングで呼ぶ．callYoutubeDataApiに関しては，URLSet時のみで良いため，ここでは呼ばない．
   useEffect(() => {
     setInputURL("")
@@ -132,14 +104,14 @@ const YoutubePage = () => {
     // currnetTime = 動画が今何秒か．
     if (currentTime != -1 && transcription_list.length != 0) {
       let index = binarySearch(transcription_list, currentTime);
-
+      
       // indexに-1が入るパターンある
       if (index < 0) index = 0;
-
+      
       handleScrollTranscript(index);
     }
   }, [currentTime]);
-
+  
   const binarySearch = (arr, currentTime) => {
     let target = currentTime
     let ok = -1;
@@ -157,7 +129,96 @@ const YoutubePage = () => {
   }
 
 
-  // ヒープ領域
+  const callYoutubeDataApi = async (v) => {
+    const api_endpoint = `https://vw0sqj3qmc.execute-api.us-east-1.amazonaws.com/stage_api?v=${v}`;
+    axios.get(api_endpoint)
+      .then((response) => {
+        if (response.data.body.error) {
+          console.log("error: ", response.data.body.message);
+        } else {
+          let title = response.data.body.title;
+          postMovie(title, v);
+        }
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      })
+  }
+
+  // URL系
+  const getYoutubeTranscript = async (v) => {
+    setIsloadingTranscript(true)
+    await fetch(`/api/get-youtube-transcript?v=${v}`)
+      .then((response) => response.json()).then(
+        (data) => {
+          // startを00:00に上書き
+          data.forEach((item) => {
+            let given_seconds = item.start;
+            let hours = Math.floor(given_seconds / 3600);
+            let minutes = Math.floor((given_seconds - (hours * 3600)) / 60);
+            let seconds = given_seconds - (hours * 3600) - (minutes * 60);
+            seconds = Math.floor(seconds);
+            item.startText = minutes.toString().padStart(2, '0') + ':' +
+              seconds.toString().padStart(2, '0');
+          })
+          makeRefList(data);
+          makeTwoDimensionalArray(data);
+          // makeMarginToTranscription1(v,data);
+        })
+      .catch((error) => {
+        setTwoDimensionalArray([[]])
+        setTranscription_list([])
+        console.log(error, 'error in getYoutubeTranscript');
+        setLabelURL("No transcript for this video.")
+        setIsloadingTranscript(false);
+        // makeRefList([{ "startText": "", "start": "Sorry,", "text": " No transcript for this video." },]);
+      }
+      );
+  }
+
+  // スクロール関係
+  const makeRefList = (array) => {
+    let refs = array.reduce((acc, value, currentIndex) => {
+      acc[currentIndex] = React.createRef();
+      return acc;
+    }, {});
+    setTranscription_list(array)
+    setRefs(refs)
+    setIsloadingTranscript(false)
+  }
+
+  // transcriptionを二次元配列化
+  const makeTwoDimensionalArray = (array) => {
+    let twoDimensionalArray = []
+    array.forEach((item) => {
+      let newText = item.text.split(" ");
+      twoDimensionalArray.push(newText);
+    })
+    setTwoDimensionalArray(twoDimensionalArray);
+    // makeMarginToTranscription2(twoDimensionalArray)
+  }
+
+  const handleScrollTranscript = (id) => {
+    // あえて字幕の上に余白を作る（過ぎた字幕も見れるため）
+    // 最初の２行だけは動かさない．
+    let start;
+    if (id > 2) {
+      start = Number(id) - 2;
+    } else {
+      start = 0;
+    }
+
+    refs[start].current.scrollIntoView({
+      behavior: 'auto',
+      block: 'start',
+    });
+    // 色かえもここで行う．
+    setActiveIndex(id);
+  }
+
+
+
+  // Word and Movie -----
   const heapPostWord = () => {
     let data = { word: word, list_id: list_id, v: video_id };
     let index = mapping[video_id];
@@ -193,8 +254,16 @@ const YoutubePage = () => {
     }
   }
 
-  // ヒープ領域
-  const getUnSavedWords = (v) => {
+  const putHeapSavedMovie = (editValue, v) => {
+    let index = mapping[v];
+    let data = { title: editValue, v: v };
+    movie_list_unregisterd[index] = data;
+    let list = movie_list_unregisterd.slice();
+    setMovie_list(list);
+  }
+
+
+  const getHeapSavedWords = (v) => {
     let list = [];
     if(v in mapping){
       let index = mapping[v];
@@ -206,7 +275,7 @@ const YoutubePage = () => {
   const getSavedWords = async (v) => {
     let all_words = [];
     if (displayName=="Unregistered"){
-      getUnSavedWords(v);
+      getHeapSavedWords(v);
     }else{
       const res = await getWordsTask(v);
       all_words = res.data;
@@ -214,91 +283,54 @@ const YoutubePage = () => {
     }
   }
 
-  // URL系
-  const getYoutubeTranscript = async (v) => {
-    setIsloadingTranscript(true)
-    await fetch(`/api/get-youtube-transcript?v=${v}`)
-      .then((response) => response.json()).then(
-        (data) => {
-          // startを00:00に上書き
-          data.forEach((item) => {
-            let given_seconds = item.start;
-            let hours = Math.floor(given_seconds / 3600);
-            let minutes = Math.floor((given_seconds - (hours * 3600)) / 60);
-            let seconds = given_seconds - (hours * 3600) - (minutes * 60);
-            seconds = Math.floor(seconds);
-            item.startText = minutes.toString().padStart(2, '0') + ':' +
-              seconds.toString().padStart(2, '0');
-          })
-          makeRefList(data);
-          makeTwoDimensionalArray(data);
-          // makeMarginToTranscription1(v,data);
-        })
-      .catch((error) => {
-        setTwoDimensionalArray([[]])
-        setTranscription_list([])
-        console.log(error, 'error in getYoutubeTranscript');
-        setLabelURL("No transcript for this video.")
-        setIsloadingTranscript(false);
-        // makeRefList([{ "startText": "", "start": "Sorry,", "text": " No transcript for this video." },]);
-      }
-      );
-  }
-
-  // 余白追加
-  // const makeMarginToTranscription1 = (v, array) => {
-  //   const margin_arr = [...Array(2)].map(() => new Object());
-  //   margin_arr.map((item) => {
-  //     item.text = ""
-  //     item.start = array[0].start
-  //     item.startText = ""
-  //   });
-
-  //   array = margin_arr.concat(array);
-  //   makeRefList(array); 
-  //   makeTwoDimensionalArray(array);
-  // }
-
-  // スクロール関係
-  const makeRefList = (array) => {
-    let refs = array.reduce((acc, value, currentIndex) => {
-      acc[currentIndex] = React.createRef();
-      return acc;
-    }, {});
-    setTranscription_list(array)
-    setRefs(refs)
-    setIsloadingTranscript(false)
-  }
-
-  // transcriptionを二次元配列化
-  const makeTwoDimensionalArray = (array) => {
-    let twoDimensionalArray = []
-    array.forEach((item) => {
-      let newText = item.text.split(" ");
-      twoDimensionalArray.push(newText);
-    })
-    setTwoDimensionalArray(twoDimensionalArray);
-    // makeMarginToTranscription2(twoDimensionalArray)
-  }
-
-  const handleScrollTranscript = (id) => {
-    // あえて字幕の上に余白を作る（過ぎた字幕も見れるため）
-    // 最初の２行だけは動かさない．
-    let start;
-    if(id > 2) {
-      start = Number(id) - 2;
-    }else{
-      start = 0;
+  // ヒープ領域
+  const heapPostMovie = (title, v) => {
+    let data = { title: title, v: v };
+    if (v in mapping) {
+      // 連想配列に既に登録済みの場合
+      // 特に何もしない
+    } else {
+      // javascriptは配列を代入すると参照渡しになる．useStateしてもstateが変わってないため，意味をなさない．そこで，sliceで値渡しにしてuseStateする.
+      movie_list_unregisterd.push(data);
+      let list = movie_list_unregisterd.slice();
+      setMovie_list(list);
+      // word_list_unregisterdにから配列を挿入
+      word_list_unregisterd.push([]);
+      // movie_list_unregisterd，word_list_unregisterdに対するmovieのindexをmappingに格納
+      mapping[v] = movie_list_unregisterd.length - 1;
     }
-    
-    refs[start].current.scrollIntoView({
-      behavior: 'auto',
-      block: 'start',
-    });
-    // 色かえもここで行う．
-    setActiveIndex(id);
   }
 
+  const postMovie = async (title, v) => {
+    if (displayName == "Unregistered") {
+      heapPostMovie(title, v);
+    } else {
+      await postMovieTask({ title: title, v: v });
+    }
+    getSavedMovies();
+  }
+
+  // ヒープ領域 
+  const getHeapSavedMovies = () => {
+    // console.log("getUnSavedMovies");
+  }
+
+  const getSavedMovies = async () => {
+    if (displayName == "Unregistered") {
+      getHeapSavedMovies();
+    } else {
+      // json形式で取得
+      const all_movies = await getMoviesTask();
+      if (all_movies.data != "") {
+        setMovie_list(all_movies.data);
+      } else {
+        // なかった場合は，初期化
+        setMovie_list([]);
+      }
+    }
+  }
+
+  // hanedl click or sumit -----
   const changeCurrentTime = (id) => {
     seekVideo(transcription_list[id].start)
   }
@@ -316,58 +348,10 @@ const YoutubePage = () => {
   }
 
   const handleClickToDisplay = () => {
-    // check
-
     setDisplay_transcription(prev => !prev);
   }
 
-  // ヒープ領域
-  const heapPostMovie = (title, v) => {
-    let data = {title: title, v:v};
-    if(v in mapping){
-      // 連想配列に既に登録済みの場合
-      // 特に何もしない
-    }else{
-      // javascriptは配列を代入すると参照渡しになる．useStateしてもstateが変わってないため，意味をなさない．そこで，sliceで値渡しにしてuseStateする.
-      movie_list_unregisterd.push(data);
-      let list = movie_list_unregisterd.slice();
-      setMovie_list(list); 
-      // word_list_unregisterdにから配列を挿入
-      word_list_unregisterd.push([]);
-      // movie_list_unregisterd，word_list_unregisterdに対するmovieのindexをmappingに格納
-      mapping[v] = movie_list_unregisterd.length - 1;
-    }
-  }
-
-  const postMovie = async (title, v) => {
-    if (displayName=="Unregistered"){
-      heapPostMovie(title,v);
-    }else{
-      await postMovieTask({ title: title, v: v });
-    }
-    getSavedMovies();
-  }
-  
-  // ヒープ領域 
-  const getHeapSavedMovies = () => {
-    // console.log("getUnSavedMovies");
-  }
-
-  const getSavedMovies = async () => {
-    if (displayName=="Unregistered"){
-      getHeapSavedMovies();
-    }else{
-      // json形式で取得
-      const all_movies = await getMoviesTask();
-      if (all_movies.data != "") {
-        setMovie_list(all_movies.data);
-      } else {
-        // なかった場合は，初期化
-        setMovie_list([]);
-      }
-    }
-  }
-
+ 
   const onSubmit = async (e) => {
     e.preventDefault();
     setIsLoadingVideo(true);
